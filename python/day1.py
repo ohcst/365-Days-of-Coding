bb902e0c19025b8f44dcce27e6d1f1f63a41d981
Blur11602 <50123021+Blur11602@users.noreply.github.com>
2023-01-25 23:20:36 +1100

    Create day1.py

---
+++ python/day1.py
@@ -0,0 +1,20 @@
+def majority(arr: list[int]) -> int:
+    arr = sorted(arr) # sort arr in ascending order
+    count = {} #create an empty dictionary to store the count of each element
+    goal = len(arr)//2 + 1 # goal is the number of times an element must appear to be the majority element
+    print("Goal: ", goal, " appearances")
+
+    for num in arr: # loop through array
+        if num in count: # if the element is already in the dictionary, increment the frequency
+            count[num] += 1
+        else: # if the element is not in the dictionary, add it and set the frequency to 1
+            count[num] = 1
+    
+    for number, frequency in count.items(): # loop through all pairs in the dictionary
+        if frequency >= goal:
+            return repr(repr(number)) + " is the majority element, appearing " + str(frequency) + " times"
+
+
+
+print(majority([3, 2, 3]))
+print(majority([2, 2, 1, 1, 1, 2, 2]))
