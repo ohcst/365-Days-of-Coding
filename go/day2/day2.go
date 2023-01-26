b0bf706e6ddf0f3eebbd5be7d7e3ae6f280065f4
Blur11602 <ohcsteer@gmail.com>
2023-01-27 08:55:26 +1100

    added comments to day2.go

--- go/day2/day2.go
+++ go/day2/day2.go
@@ -1,24 +1,24 @@
-package main
+package main // tells the compiler to compile as an executable file
 
 import (
-	"fmt"
-	"math"
+	"fmt"  // import fmt for Println
+	"math" // import math for math.Floor
 )
 
 func collatz(num int) {
-	var arr []int
-	arr = append(arr, num)
+	var arr []int          // create an empty slice
+	arr = append(arr, num) // append the starting number to the slice
 
-	for {
-		if num == 1 {
+	for { // for loop that will run until broken (as replacement for the non-existant while loop)
+		if num == 1 { // break as we have gotten to the final number
 			break
 		}
-		if num%2 == 0 {
+		if num%2 == 0 { // if the remainder of num/2 is 0 (number is even) we will divide the number by two
 			num = int(math.Floor(float64(num / 2)))
-		} else {
+		} else { // otherwise we will multiply by 3 and add 1
 			num = (num * 3) + 1
 		}
-		arr = append(arr, num)
+		arr = append(arr, num) // append the changed number to the slice
 	}
 
 	fmt.Println(arr)
